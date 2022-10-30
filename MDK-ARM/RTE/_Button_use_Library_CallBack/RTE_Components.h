4126556cae343abf547280bff3599b6e2f4c6400
Admin <Admin@ADMIN>
2022-10-30 10:41:41 +0700

    First commit

---
+++ MDK-ARM/RTE/_Button_use_Library_CallBack/RTE_Components.h
@@ -0,0 +1,21 @@
+
+/*
+ * Auto generated Run-Time-Environment Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'Button_use_Library_CallBack' 
+ * Target:  'Button_use_Library_CallBack' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "stm32f10x.h"
+
+
+
+#endif /* RTE_COMPONENTS_H */
